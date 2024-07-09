505529f820d829adaa16c7832f8dabca3cb0a025
Abhishek Anand <yeyeabhishekanand.com>
2024-07-10 00:27:21 +0530

    added comments and Improved code readibility

--- src/Arena.js
+++ src/Arena.js
@@ -1,12 +1,20 @@
 const Player = require('./Player');
 
 class Arena {
+     /**
+   * Creating a new Arena for a battle.
+   * @param {Player} playerA - The first player.
+   * @param {Player} playerB - The second player.
+   */
     constructor(player1, player2) {
         this.player1 = player1;
         this.player2 = player2;
         this.turnCounter = 1;
     }
-
+    
+     /**
+   * Start the battle and simulate turns until one player wins.
+   */
     fight() {
         let attacker = this.player1.health <= this.player2.health ? this.player1 : this.player2;
         let defender = attacker === this.player1 ? this.player2 : this.player1;
@@ -25,6 +33,11 @@ class Arena {
         return winner;
     }
 
+    /**
+   * Simulating a single turn of battle.
+   * @param {Player} attacker - The attacking player.
+   * @param {Player} defender - The defending player.
+   */
     turn(attacker, defender) {
         const attackingRoll = attacker.diceRoll();
         const defendingRoll = defender.diceRoll();
--- src/Player.js
+++ src/Player.js
@@ -1,4 +1,10 @@
 class Player {
+    /**
+   * Creating new Player.
+   * @param {string} name - The name of the player.
+   * @param {number} health - The health points of the player.
+   * @param {number} strength - The strength multiplier of the player.
+   */
     constructor(name, health, strength, attack) {
         if (health <= 0 || strength <= 0 || attack <= 0) {
             throw new Error("Health, strength, and attack must be positive integers.");
@@ -9,16 +15,28 @@ class Player {
         this.attack = attack;
     }
 
+    /**
+     * Checking if the player is still alive.
+     * @returns {boolean} - True if the player's health is greater than 0, false otherwise.
+     */
     isAlive() {
         return this.health > 0;
     }
 
+    /**
+   * Simulating a dice roll for the player.
+   * @returns {number} - The result of the dice roll (1-6).
+   */
     diceRoll() {
         const roll = Math.floor(Math.random() * 6) + 1;
         console.log(`Dice Roll for ${this.name}: ${roll}`);
         return roll;
     }
 
+    /**
+   * Reducing the player's health by a specified amount of damage.
+   * @param {number} damage - The amount of damage to be subtracted from health.
+   */
     takeDamage(damage) {
         this.health = Math.max(this.health - damage, 0);
     }
